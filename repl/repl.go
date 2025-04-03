18fe374b44ab9ba45c122892eb93380aff797849
ChaosNyaruko <cabbageparadise@gmail.com>
2025-04-03 14:31:39 +0800

    make the repl fancier

--- repl/repl.go
+++ repl/repl.go
@@ -6,13 +6,29 @@ import (
 	"io"
 
 	"github.com/ChaosNyaruko/monkey/lexer"
+	"github.com/ChaosNyaruko/monkey/parser"
 	"github.com/ChaosNyaruko/monkey/token"
 )
 
 const PROMPT = ">>"
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 // read-evaluate-print loop
 func Start(in io.Reader, out io.Writer) error {
+	// TODO: use GNU readline shortcuts?
+	fmt.Fprintf(out, MONKEY_FACE)
 	scanner := bufio.NewScanner(in)
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -23,8 +39,28 @@ func Start(in io.Reader, out io.Writer) error {
 		line := scanner.Text()
 		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		if false {
+			// TODO: options
+			for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+				fmt.Fprintf(out, "%+v\n", tok)
+			}
 		}
+
+		p := parser.New(l)
+		program := p.ParseProgram()
+		errs := p.Errors()
+		if len(errs) != 0 {
+			printErrors(out, errs)
+			continue
+		}
+
+		// evaluate: print the well-formed AST
+		fmt.Fprintf(out, "%s\n", program.String())
+	}
+}
+
+func printErrors(out io.Writer, errs []string) {
+	for _, msg := range errs {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
